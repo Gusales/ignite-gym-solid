a7e42bc9ed89a570bb74270c9c510a10880a71d8
= <=>
2023-09-21 18:04:29 -0300

    Refactor: Refactoring instances in tests
    Update instances to be used in all tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,15 +1,19 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
-// import { PrismaUserRepository } from '@/repositories/prisma/prisma-user-repository'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-describe('Register Use Case', () => {
-  it('should be able to register', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new RegisterUseCase(usersRepository)
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
 
+describe('Register Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(usersRepository)
+  })
+
+  it('should be able to register', async () => {
     const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@dev.com',
@@ -20,9 +24,6 @@ describe('Register Use Case', () => {
   })
 
   it(`The user's password must be hashed before being registered`, async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new RegisterUseCase(usersRepository)
-
     const {
       user: { password_hash },
     } = await sut.execute({
@@ -37,9 +38,6 @@ describe('Register Use Case', () => {
   })
 
   it('should not be able to register with same email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new RegisterUseCase(usersRepository)
-
     const email = 'node@dev.com'
 
     await sut.execute({

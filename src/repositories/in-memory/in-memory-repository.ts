9fb9e123fa9ae1f7c9636f30dfc48aacd9400b63
= <=>
2023-09-21 20:25:20 -0300

    Feat: Create use case profile

--- src/repositories/in-memory/in-memory-repository.ts
+++ src/repositories/in-memory/in-memory-repository.ts
@@ -4,13 +4,23 @@ import { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public Users: User[] = []
 
+  async findById(id: string) {
+    const user = this.Users.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = this.Users.find((item) => item.email === email)
 
-    if (user) {
-      return user
+    if (!user) {
+      return null
     }
-    return null
+    return user
   }
 
   async create(data: Prisma.UserCreateInput) {
